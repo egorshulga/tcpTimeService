5273fa84f49a0e667ad7d39ae9c1c1820df62044
Egor Shulga <scrilan@gmail.com>
2015-03-03 20:33:30 +0300

    project start

---
+++ tcpTimeService/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace tcpTimeService
+{
+	static class Program
+	{
+		static void Main(string[] args)
+		{
+			if (!args.Any())
+			{
+				Console.WriteLine("Choose either server or client to start. Terminating");
+				return;
+			}
+			switch (args[1])
+			{
+				case "server":
+					ServerStart();
+					break;
+				case "client":
+					ClientStart();
+					break;
+				default:
+					Console.WriteLine("Choose either server or client to start. Terminating");
+					break;
+			}
+		}
+
+
+		private static void ServerStart()
+		{
+			const int port = 1414;
+			TcpListener server = new TcpListener(IPAddress.Any, port);
+			try
+			{
+				server.Start();
+
+				while (true)
+				{
+					Console.WriteLine("Server: waiting for connections...");
+					TcpClient client = server.AcceptTcpClient();
+					Console.WriteLine("Server: ");
+				}
+
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Error {0}", e);
+			}
+			finally
+			{
+				server.Stop();
+			}
+		}
+
+
+		private static void ClientStart()
+		{
+			throw new NotImplementedException();
+		}
+
+
+	}
+}
